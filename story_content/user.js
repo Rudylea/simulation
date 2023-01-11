2d988108cdd974fed8593d91ca67baaedf659cd5
Rudylea <121963489+Rudylea@users.noreply.github.com>
2023-01-11 12:36:58 +0100

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6VXovdb96w6":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+audio.src=
+audio.load();
+audio.play();
+}
+
